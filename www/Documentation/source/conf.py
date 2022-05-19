37f9a17b61b471fa47329664d33cba9c6991f0b2
Henrique Rodrigues <henrique.rodrigues@etu.iut-tlse3.fr>
2022-05-19 11:14:09 +0200

    cancel

--- www/Documentation/source/conf.py
+++ www/Documentation/source/conf.py
@@ -82,10 +82,8 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "rightsidebar": "true",
-    "relbarbgcolor": "black"
-}
+# html_theme_options = {}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
